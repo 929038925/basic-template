2f16541c4aedb9170a2e5cde5abb59afe5df07f9
何瀚柯 <929038925@qq.com>
2022-06-13 10:39:30 +0800

    格式化样式

--- src/views/[...notFound].vue
+++ src/views/[...notFound].vue
@@ -1,18 +1,15 @@
 <template>
-<div class="not-found">
-你所访问的页面不存在
-</div>
+  <div class="not-found">你所访问的页面不存在</div>
 </template>
 
 <script lang='ts' setup>
-
 </script>
 
 <style lang='scss' scoped>
-.not-found{
+.not-found {
   width: 100%;
   height: 100%;
-  padding: 15% 0 ;
+  padding: 15% 0;
   box-sizing: border-box;
   text-align: center;
   background-color: rgb(212, 212, 255);
--- src/views/foo.vue
+++ src/views/foo.vue
@@ -1,13 +1,9 @@
 <template>
-<div>
-foo
-</div>
+  <div>foo</div>
 </template>
 
 <script setup lang="ts">
-
 </script>
 
 <style lang='scss' scoped>
-
 </style>
